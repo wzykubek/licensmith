85d46d6706515cb474d45ba2234435200bbd0d6f
Wiktor Zykubek <dev@wzykubek.xyz>
2025-02-17 21:19:54 +0100

    feat: add version command

---
+++ cmd/version.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	version = "dev"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Show the version",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Licensmith v%s\n", version)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
